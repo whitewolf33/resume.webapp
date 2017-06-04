88b82792c242112d88a704564bddcfb0adf3da45
Vinod Srinivasan <whitewolf33@gmail.com>
2017-06-04 20:12:34 +1000

    NodeService debugging

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.NodeServices;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +31,9 @@ namespace resume_webapp
         {
             // Add framework services.
             services.AddMvc();
+            services.AddNodeServices(new NodeServicesOptions {
+                LaunchWithDebugging = true
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
